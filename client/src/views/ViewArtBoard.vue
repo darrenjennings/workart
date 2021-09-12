059c8a865747fb309786ecf8b65df8f1d7ddcb8f
Darren Jennings <dmjenn02@gmail.com>
2021-09-12 16:43:24 -0400

    fixup! fourth

---
+++ client/src/views/ViewArtBoard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container flex justify-center">
+    <Art
+      class="w-full"
+      v-if="artboard !== null"
+      :artboard="artboard"
+      :color="color"
+      readonly
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { useRouter, useRoute } from "vue-router";
+
+import Art from "../components/Art.vue";
+import Api from "../service/api";
+import type { ArtBoard } from "../components/Art.vue";
+import { onMounted, ref, inject } from "vue";
+
+export default {
+  components: { Art },
+  setup(props, config) {
+    const route = useRoute();
+    const $api: Api = inject("$api");
+    const artboard = ref(null);
+    const color = ref("#000000");
+
+    onMounted(() => {
+      $api.get(`/api/${route.params.date}`).then((res) => {
+        artboard.value = res;
+      });
+    });
+
+    return {
+      artboard,
+      color,
+    };
+  },
+};
+</script>
